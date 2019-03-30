018759f406daad1c8360885d938d636261d42725
chrfilip <chrfilip@outlook.com>
2019-03-30 23:56:14 +0200

    Added base classes for screenshot testing

---
+++ app/src/androidTest/java/com/xm/screenshot/testing/TestDevice.kt
@@ -0,0 +1,25 @@
+package com.xm.screenshot.testing
+
+import android.view.ViewGroup
+import java.util.*
+
+/**
+ * Created by Christoforos Filippou on 30/03/2019
+ */
+data class TestDevice(val width: Int, val height: Int) {
+
+    lateinit var info: String
+
+    lateinit var locale: Locale
+
+    var isDarkTheme: Boolean = false
+
+    constructor(width: Int, height: Int, isDarkTheme: Boolean, locale: Locale) : this(width, height) {
+        // avoid showing constant value of WRAP_CONTENT in screenshot file names
+        val heightText = if (height == ViewGroup.LayoutParams.WRAP_CONTENT) "WRAP_CONTENT" else height.toString()
+
+        this.info = width.toString() + "x" + heightText + "_" + locale.displayName + "_" + if (isDarkTheme) "DarkTheme" else "LightTheme"
+        this.locale = locale
+        this.isDarkTheme = isDarkTheme
+    }
+}
